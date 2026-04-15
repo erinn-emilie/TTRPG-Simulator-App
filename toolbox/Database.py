e2e6d4de3dd1765ac7c4f8a717987ba188afe846
erinn-emilie <erinnemiliek@gmail.com>
2026-04-14 23:23:51 -0400

    lots and lots of changes

--- toolbox/Database.py
+++ toolbox/Database.py
@@ -2,6 +2,8 @@ import bcrypt
 import urllib
 from enum import Enum
 
+import requests
+
 from sqlalchemy import create_engine, Column, Integer, String, select, ForeignKey, LargeBinary
 from sqlalchemy.orm import declarative_base, sessionmaker
 
@@ -10,64 +12,9 @@ from PIL import Image
 import io  
 
 from Enums.TokenTypes import TokenTypes
- 
 
 
-driver = "ODBC Driver 17 for SQL Server"  
-server = r"localhost\SQLEXPRESS"      
-database = "TTRPGDB"
-
-params = urllib.parse.quote_plus(
-    f"DRIVER={{{driver}}};"
-    f"SERVER={server};"
-    f"DATABASE={database};"
-    f"Trusted_Connection=yes;"
-)
-
-engine = create_engine(f"mssql+pyodbc:///?odbc_connect={params}")
-
-
-SessionLocal = sessionmaker(bind=engine, autoflush=False, autocommit=False, future=True)
-
-Base = declarative_base()
-
-class Hosts(Base):
-    __tablename__ = "Hosts"
-    connection_id = Column(Integer, primary_key=True)
-    hostname = Column(String(100), nullable=False)
-    fk_user_id = Column(Integer, ForeignKey("Users.user_id"), nullable=False)
-    password = Column(String(60), nullable=False)
-    port = Column(Integer, nullable=False)
-
-class Users(Base):
-    __tablename__ = "Users"
-    user_id = Column(Integer, primary_key=True)
-    username = Column(String(50), nullable=False)
-    password = Column(String(60), nullable=False)
-    email = Column(String(100), nullable=False)
-
-class Maps(Base):
-    __tablename__ = "Maps"
-    map_id = Column(Integer, primary_key=True)
-    fk_user_id = Column(Integer, ForeignKey("Users.user_id"), nullable=False)
-    map_name = Column(String(100), nullable=False)
-    map_dict = Column(String(), nullable=False)
-
-class Tokens(Base):
-    __tablename__ = "Tokens"
-    token_id = Column(Integer, primary_key=True)
-    fk_user_id = Column(Integer, ForeignKey("Users.user_id"), nullable=False)
-    user_token_key = Column(Integer, nullable=False)
-    token_name = Column(String(200), nullable=False)
-    token_type = Column(String(20), nullable=False)
-    json_small_fields = Column(String(), nullable=False)
-    json_large_fields = Column(String(), nullable=False)
-    img_1 = Column(LargeBinary, nullable=True)
-    img_2 = Column(LargeBinary, nullable=True)
-    img_3 = Column(LargeBinary, nullable=True)
-    img_4 = Column(LargeBinary, nullable=True)
-    img_5 = Column(LargeBinary, nullable=True)
-    map_asset = Column(LargeBinary, nullable=True)
+URL = "probably shouldnt have this in github lol"
 
 class DatabaseMessages(Enum):
     NONE = -1
@@ -76,10 +23,24 @@ class DatabaseMessages(Enum):
     EXISTING_EMAIL = 2
     CRITICAL_ERROR = 3
     INVAL_CREDS = 4
-    EXISTING_MAP = 5
+    DUPLICATE = 5
     NO_SESSION_FOUND = 6
 
+
 class Database:
+    def check_user(username:str, password:str):
+        url = f"{URL}/check-account"
+        response = requests.post(url, json={"username": username, "password": password})
+        json_response = response.json()
+        message = json_response["message"]
+        if("ERROR" in message or "NONE" in message):
+            return DatabaseMessages.CRITICAL_ERROR, -1
+        elif("INVAL CREDENTIALS" in message):
+            return DatabaseMessages.INVAL_CREDS, -1
+        else:
+            uid = json_response["id"]
+            return DatabaseMessages.SUCCESS, uid
+    
     def compress_image(image_path, quality=85):  
         with Image.open(image_path) as img:  
             if img.mode in ("RGBA", "P"):  
@@ -89,138 +50,37 @@ class Database:
             return img_byte_arr.getvalue() 
 
     def add_new_token(user_id:int, user_token_key:int, token_name:str, token_type:TokenTypes, json_small_fields:dict, json_large_fields:dict, map_asset="", images=[]):
-        with SessionLocal() as session:
-            new_token = Tokens(
-                fk_user_id=user_id,
-                user_token_key=user_token_key,
-                token_name=token_name,
-                token_type=TokenTypes.get_str_from_token_type(token_type),
-                json_small_fields=str(json_small_fields),
-                json_large_fields=str(json_large_fields)
-            )
-            session.add(new_token)
-            session.commit()
-            if(map_asset != ""):
-                new_token.map_asset = map_asset
-            for idx, img in images:
-                compressed = Database.compress_image(img)
-                match(idx):
-                    case 1:
-                        new_token.img_1 = compressed
-                    case 2:
-                        new_token.img_2 = compressed
-                    case 3:
-                        new_token.img_3 = compressed
-                    case 4:
-                        new_token.img_4 = compressed
-                    case 5: 
-                        new_token.img_5 = compressed
-            session.commit()
+        compressed_images = []
+        for img in images:
+            compressed_images.append(Database.compress_image(img))
 
+        url = f"{URL}/add-token"
+        response = requests.post(url, json={"user_id": user_id, "token_name": token_name, "token_type": token_type, "json_small_fields": str(json_small_fields), "json_large_fields": str(json_large_fields), "images": compressed_images, "map_asset": map_asset})
+        json_response = response.json()
+        message = json_response["message"]
+        if("ERROR" in message or "NONE" in message):
+            return DatabaseMessages.CRITICAL_ERROR
+        elif("DUPLICATE" in message):
+            return DatabaseMessages.DUPLICATE
+        else:
+            return DatabaseMessages.SUCCESS
+        
 
     def add_host_info_to_db(hostname:str, user_id:int, password:str, port:int):
-        with SessionLocal() as session:
-            new_host = Hosts(
-                hostname=hostname,
-                fk_user_id=user_id,
-                password=password,
-                port=port
-            )
-            session.add(new_host)
-            session.commit()
+        url = f"{URL}/add-host"
+        response = requests.post(url, json={"hostname": hostname, "user_id": user_id, "password": password, "port": port})
+        json_response = response.json()
+        message = json_response["message"]
 
-    def get_host_info(username:str, password:str):
-        msg = DatabaseMessages.NONE
-        try:
-            with SessionLocal() as session:
-                user = session.execute(
-                    select(Users).where(Users.username == username)
-                ).scalar_one_or_none()
-                if(user):
-                    info = session.execute(
-                        select(Hosts).where(Hosts.fk_user_id == user.user_id)
-                    ).scalar_one_or_none()
-                    if(info):
-                        if(bcrypt.checkpw(password.encode("utf-8"), info.password.encode("utf-8"))):
-                            msg = DatabaseMessages.SUCCESS
-                            return msg, session.hostname, session.port
-                        else:
-                            msg = DatabaseMessages.NO_SESSION_FOUND
-                    else:
-                        msg = DatabaseMessages.NO_SESSION_FOUND
-                else:
-                    msg = DatabaseMessages.NO_SESSION_FOUND
-            return msg, "", ""
-        except Exception as e:
-            msg = DatabaseMessages.CRITICAL_ERROR
-            return msg, "", ""
-
-
-
-    def add_map_to_db(user_id:int, map_name:str, map_dict:dict):
-        msg = DatabaseMessages.NONE
-        try:
-            with SessionLocal() as session:
-                existing_map = session.execute(
-                    select(Maps).where(Maps.map_name == map_name)
-                ).scalar_one_or_none()
-                if existing_map:
-                    msg = DatabaseMessages.EXISTING_MAP
-                else:
-                    new_map = Maps(
-                        map_name=map_name,
-                        fk_user_id=user_id,
-                        map_dict=str(map_dict)
-                    )
-                    session.add(new_map)
-                    session.commit()
-                    msg = DatabaseMessages.SUCCESS
-            return msg
-        except Exception as e:
-            msg = DatabaseMessages.CRITICAL_ERROR
-            return msg
-
-    def create_new_user(username:str, email:str, password:str) -> DatabaseMessages:
-        msg = DatabaseMessages.NONE
-        pw_hash = bcrypt.hashpw(password.encode("utf-8"), bcrypt.gensalt()).decode("utf-8")
-
-        try:
-            with SessionLocal() as session:
-                existing_user = session.execute(
-                    select(Users).where(Users.username == username)
-                ).scalar_one_or_none()
-                if existing_user:
-                    msg = DatabaseMessages.EXISTING_USER
-
-                existing_email = session.execute(
-                    select(Users).where(Users.email == email)
-                ).scalar_one_or_none()
-                if existing_email:
-                    msg = DatabaseMessages.EXISTING_EMAIL
-
-                session.add(Users(username=username, email=email, password=pw_hash))
-                session.commit()
-                return Database.check_user(username, password)
-            return msg, -1
-        except Exception as e:
-            msg = DatabaseMessages.CRITICAL_ERROR
-            return msg, -1
-
-
-    def check_user(username:str, password:str):
-        msg = DatabaseMessages.NONE
-        try:
-            with SessionLocal() as session:
-                user = session.execute(
-                    select(Users).where(Users.username == username)
-                ).scalar_one_or_none()
-
-                if user and bcrypt.checkpw(password.encode("utf-8"), user.password.encode("utf-8")):
-                    msg = DatabaseMessages.SUCCESS
-                    return msg, user.user_id
-                else:
-                    msg = DatabaseMessages.INVAL_CREDS
-                    return msg, -1
-        except Exception as e:
-            msg = DatabaseMessages.CRITICAL_ERROR
-            return msg, -1
+    def create_new_user(username:str, email:str, password:str):
+        url = f"{URL}/create-account"
+        response = requests.post(url, json={"username": username, "email": email, "password": password})
+        json_response = response.json()
+        message = json_response["message"]
+        if("ERROR" in message):
+            return DatabaseMessages.CRITICAL_ERROR, -1
+        elif("INVAL CREDENTIALS" in message):
+            return DatabaseMessages.INVAL_CREDS, -1
+        else:
+            uid = json_response["id"]
+            return DatabaseMessages.SUCCESS, uid
